8d2e331156387b6fc5c595be785e32efc79129aa
Digidai <daiq@live.cn>
2025-07-16 18:20:05 +0800

    Add Google Search Console verification meta tag

---
+++ src/pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="google-site-verification" content="X6O3dYGKI5pMYaUMyKI357kkZz7tjPnuY-SU1vFTjUs" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+} 
