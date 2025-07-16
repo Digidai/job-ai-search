759d69fda95ec710f5dab9983e5fd08849eff743
Digidai <daiq@live.cn>
2025-07-16 18:25:51 +0800

    Add Bing Webmaster Tools verification meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,7 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="google-site-verification" content="X6O3dYGKI5pMYaUMyKI357kkZz7tjPnuY-SU1vFTjUs" />
+        <meta name="msvalidate.01" content="98D5A95B06A44D38F6B3F672F5FF6B98" />
       </Head>
       <body>
         <Main />
